129e7577e9f4df2cd9f74076a944299a0bdfc134
alyconr <alyconr@hotmail.com>
2023-10-20 07:48:24 -0500

    :zap: Adding user and job controllers

---
+++ controllers/auth.js
@@ -0,0 +1,14 @@
+const register = async (req, res) => {
+    res.send('Register User')
+}
+
+
+const login = async (req, res) => {
+    res.send('login User')
+}
+
+
+module.exports = {
+    register,
+    login
+}
