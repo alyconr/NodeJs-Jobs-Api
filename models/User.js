891b739834f42b7b67c9dcfcfd23e4625d8e72be
alyconr <alyconr@hotmail.com>
2023-10-20 07:55:33 -0500

    :wrench Configure  db models

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide name'],
+        minlength: 3,
+        maxlength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide email'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide valid email',
+        ],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide password'],
+        minlength: 6,
+        maxlength: 12,
+        match: [
+            /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, 
+            'Please provide valid password',
+        ]
+
+
+    },
+
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
