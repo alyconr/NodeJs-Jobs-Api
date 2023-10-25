675f2b54dd2e5e700c55bf2578931b14eb67034a
alyconr <alyconr@hotmail.com>
2023-10-24 19:25:48 -0500

    :zap: Adding security middlewares and rate limiters

--- app.js
+++ app.js
@@ -1,40 +1,61 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
 const app = express();
 
+//security packages
 
-const connectDB = require('./db/connect');
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
 
+const connectDB = require("./db/connect");
+const autheticateUser = require("./middleware/authentication");
 
-const authRouter = require('./routes/auth');
-const jobsRouter = require('./routes/jobs');
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
 
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
+app.use(cors());
+app.use(xss());
+app.use(helmet());
+app.set("trust proxy", true); // trust proxy is needed to veryify  if the request is coming from a real  server
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 
-
-
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", autheticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobsRouter);
+app.get("/", (req, res) => {
+  res.send("Jobs API");
+});
 
-
-const port = process.env.PORT || 5000;
-
+const port = process.env.PORT || 8000;
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port, console.log(`Server is listening on port ${port} and connected to db`));
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(
+      port,
+      console.log(`Server is listening on port ${port} and connected to db`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
