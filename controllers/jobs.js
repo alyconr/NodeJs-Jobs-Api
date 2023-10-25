ca60733481f2ea6164cb3596e05bfbc85ad72676
alyconr <alyconr@hotmail.com>
2023-10-24 19:27:56 -0500

    :sparkles: adding  all the cruds

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,28 +1,82 @@
-const getAllJobs = async (req, res) => {
-    res.send('getAllJobs')
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
-}
+const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
+};
 
 const getJob = async (req, res) => {
-    res.send('getJob')
-}
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) {
+    throw new UnauthenticatedError("Job not found");
+  }
+
+  res.status(StatusCodes.OK).json({ job });
+};
 
 const createJob = async (req, res) => {
-    res.send('createJob')
-}
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
 
 const updateJob = async (req, res) => {
-    res.send('updateJob')
-}
+  const {
+    user: { userId },
+    params: { id: jobId },
+    body: { company, position },
+  } = req;
+
+  if (company === "" || position === "") {
+    throw new BadRequestError("Company or Position fields cannot be empty");
+  }
+
+  const job = await Job.findOneAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!job) {
+    throw new UnauthenticatedError("Job not found");
+  }
+
+  res.status(StatusCodes.OK).json({ job });
+};
 
 const deleteJob = async (req, res) => {
-    res.send('deleteJob')
-}
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOneAndDelete({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) {
+    throw new UnauthenticatedError("Job not found");
+  }
+
+  res.status(StatusCodes.OK).json({ job });
+};
 
 module.exports = {
-    getAllJobs,
-    getJob,
-    createJob,
-    updateJob,
-    deleteJob
-}
+  createJob,
+  deleteJob,
+  getAllJobs,
+  updateJob,
+  getJob,
+};
