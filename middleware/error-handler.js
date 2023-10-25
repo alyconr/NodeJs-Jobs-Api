6af1adae539eed8cccca9d0dd9177196e0511bac
alyconr <alyconr@hotmail.com>
2023-10-24 19:31:19 -0500

    :white_check_mark: Update and customize  mongoose error handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,32 @@
-const {CustomAPIError} = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    if(err instanceof CustomAPIError){
-        return res.status(err.statusCode).json({msg: err.message})
-    }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: 'Something went wrong, please try again'})
-}
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong try again later",
+  };
 
-module.exports = errorHandlerMiddleware
+  if (err.name === "ValidationError") {
+    console.log(Object.values(err.errors));
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.code && err.code === 11000) {
+    // if duplicate key error, 11000 is the code for duplicate key
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id : ${err.value}`;
+    customError.statusCode = StatusCodes.NOT_FOUND;
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg, err });
+};
+
+module.exports = errorHandlerMiddleware;
