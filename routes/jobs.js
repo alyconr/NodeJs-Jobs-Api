2141016e26317e27293ec695b95a6dc67bb4bee1
alyconr <alyconr@hotmail.com>
2023-10-24 19:32:49 -0500

    :art: Formatting code

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
+const {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+} = require("../controllers/jobs");
 
-const {getAllJobs, getJob, createJob, updateJob, deleteJob} = require('../controllers/jobs')
+router.route("/").post(createJob).get(getAllJobs);
+router.route("/:id").get(getJob).patch(updateJob).delete(deleteJob);
 
-
-router.route('/').get(getAllJobs).post(createJob)
-router.route('/:id').get(getJob).patch(updateJob).delete(deleteJob)
-
-
-
-
-module.exports = router
+module.exports = router;
