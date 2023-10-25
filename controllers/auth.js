7a403b337464bb81f9431d56357ed827ce91c069
alyconr <alyconr@hotmail.com>
2023-10-24 19:27:13 -0500

    :sparkles: adding  login and register validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,46 @@
-const register = async (req, res) => {
-    res.send('Register User')
-}
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+
+  const candidatePassword = req.body.password;
+
+  const isMatch = await user.comparePassword(candidatePassword);
+
+  if (!isMatch) {
+    throw new BadRequestError("Invalid Credentials");
+  }
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+};
 
 const login = async (req, res) => {
-    res.send('login User')
-}
+  const { email, password } = req.body;
 
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email }); // find user by email and password combination in database
+
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+};
 
 module.exports = {
-    register,
-    login
-}
+  register,
+  login,
+};
